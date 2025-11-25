0e56d52a5fab9da088d4a1fd847eb9d592afc118
Alisher <zumanovaliser814@gmail.com>
2025-11-25 12:19:17 +0600

    401

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "@/features/authentication/store/auth";
 
 export const api = axios.create({
   baseURL: "http://92.62.72.168:50555",
@@ -13,3 +14,21 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
+
+let isLoggingOut = false;
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error?.response?.status;
+
+    if (status === 401 && !isLoggingOut) {
+      isLoggingOut = true;
+
+      const { logout } = useAuthStore.getState();
+      logout();
+    }
+
+    return Promise.reject(error);
+  }
+);
