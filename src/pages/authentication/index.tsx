d1488fde1c48f2ff7f6418b611a3cfcbf2a39e73
Alisher <zumanovaliser814@gmail.com>
2026-02-08 20:45:16 +0600

    widgets

--- src/pages/authentication/index.tsx
+++ src/pages/authentication/index.tsx
@@ -1,30 +1,3 @@
-import { useLocation, useNavigate } from "react-router";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { SignInForm } from "@/features/authentication/ui/sign-in-form";
-import { ForgotForm } from "@/features/authentication/ui/forgot-form";
+import { AuthenticationWidget } from "@/widgets/authentication";
 
-export const Authentication = () => {
-  const { pathname } = useLocation();
-
-  const navigate = useNavigate();
-
-  const isForgot = pathname.includes("forgot");
-
-  return (
-    <Box sx={{ height: "100vh", position: "relative" }}>
-      {isForgot && (
-        <Button
-          startIcon={<ArrowBackIcon />}
-          onClick={() => navigate(-1)}
-          sx={{ position: "absolute", top: 16, left: 16 }}
-        >
-          Назад
-        </Button>
-      )}
-
-      {isForgot ? <ForgotForm /> : <SignInForm />}
-    </Box>
-  );
-};
+export const Authentication = () => <AuthenticationWidget />;
