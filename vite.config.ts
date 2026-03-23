f406830b3401127b5f9cbcac72f03b7939a7a3ee
Alisher <zumanovaliser814@gmail.com>
2026-03-23 16:23:20 +0600

    save

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 import { VITE_ALIASES } from "./vite/aliases";
+import { getVendorChunkName } from "./vite/chunks";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: VITE_ALIASES,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: getVendorChunkName,
+      },
+    },
   },
 });
